1c420da457e969ae1eba750647748e5223b30ba1
Joshua Keifer <lewlewkeifer@gmail.com>
2023-12-07 14:01:26 -0500

    minor changes that did aboslutely nothing

--- app/TicketSystem/src/main/java/local/airsquatters/TicketSystem/Ticket.java
+++ app/TicketSystem/src/main/java/local/airsquatters/TicketSystem/Ticket.java
@@ -75,6 +75,7 @@ public final class Ticket {
     public String toString() {
         return "Event: " + this.getEvent() +
             "\nOwner: " + this.getOwner().toString() +
-            "\nTicket Number: " + this.getTicketNumber();
+            "\nTicket Number: " + this.getTicketNumber() +
+            "\nSeat: " + this.seat.toString();
     }
 }
--- app/ui/src/index.js
+++ app/ui/src/index.js
@@ -11,7 +11,6 @@ import PurchasePage from './PurchaseTickets.js'
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LogInPage/>}/>
@@ -22,5 +21,4 @@ root.render(
         <Route path='/purchase' element={<PurchasePage/>}/>
       </Routes>
     </BrowserRouter>
-  </StrictMode>
 );
